35d47712df4b6d384dc842e36c87d7e95a68b988
Timur <Timur@TIMURSLAPTOP>
2023-03-04 12:39:41 +0700

    Добавьте файлы проекта.

---
+++ 04.03/Cat.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi
+{
+    public struct Fruit
+    { // структура Фрукт
+        public string type;
+        public int health;
+    }
+    public class Cat
+    {
+        public string name;
+        public int hunger;
+
+        public void Eat(Fruit f) // кошка может есть фрукт
+        {
+            hunger -= f.health; // уменьшаем значение поля hunger на значение health фрукта
+            Console.WriteLine($"Cat eats {f.type}. Hunger: {hunger}");
+        }
+        public void CheckHunger()
+        {
+            Console.WriteLine($"Hunger: {hunger}");
+        }
+
+        public void Play()
+        {
+            Console.WriteLine($"{name} играет и прыгает!");
+            hunger += 5;
+            if (hunger > 0)
+            {
+                Console.WriteLine($"{name} погиб от голода!");
+            }
+            else
+            {
+                int randomWaitTime = new Random().Next(1, 15);
+                Console.WriteLine($"{name} устала и отдыхает {randomWaitTime} минут...");
+                CheckHunger();
+            }
+        }
+
+        public void Run()
+        {
+            Console.WriteLine($"{name} бегает!");
+            hunger += 1;
+            if (hunger > 0)
+            {
+                Console.WriteLine($"{name} погиб от голода!");
+            }
+            else
+            {
+                int randomWaitTime = new Random().Next(1, 15);
+                Console.WriteLine($"{name} устала и отдыхает {randomWaitTime} минут...");
+                CheckHunger();
+            }
+        }
+        public bool IsAlive()
+        {
+            bool flag = true;
+            if (hunger > 0)
+            {
+                flag = false;
+            }
+            return flag;
+        }
+
+    }
+}
---
+++ 04.03/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.Common;
+
+namespace Tamagotchi
+{
+    class Programm
+    {
+        static void Main()
+        {
+            // создали кошку
+            Cat cat = new Cat { name = "Pushok", hunger = 0 };
+            Fruit pivko = new Fruit { type = "баутика", health = 5 };
+            DoMeal(cat, pivko); // покормили кошку пивком
+
+            
+            while (cat.IsAlive())
+            {
+                int code = int.Parse(Console.ReadLine());
+                switch (code)
+                {
+                    case 1:
+                        Console.WriteLine("Вызов метода игры.");
+                        cat.Play();
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Вызов метода бега.");
+                        cat.Run();
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Вызов метода просмотра голода.");
+                        break;
+
+                }
+            }
+        }
+        static void DoMeal(Cat c, Fruit f)
+        {
+            c.Eat(f); // кошка {имя кошки} съела фрукт
+        }
+    }
+}
